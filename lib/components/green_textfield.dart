d574b8fb93feb985378fc003ab7e76368ba510bf
mnemosynememoiree <mnemosynejacasu@gmail.com>
2025-11-14 00:04:32 +0800

    login_page (1)

---
+++ lib/components/green_textfield.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class GreenTextfield extends StatelessWidget {
+  final controller;
+  final hintText;
+  final bool obscureText;
+
+  const GreenTextfield({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        cursorColor: Colors.green.shade700.withOpacity(0.7),
+        style: TextStyle(
+          color: Colors.green.shade700,
+        ),
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(color: Colors.green.shade900),
+          ),
+          fillColor: Colors.white,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.green.shade700.withOpacity(0.5)),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:fyllens/components/green_textfield.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+          
+              // logo
+              Icon(
+                  Icons.energy_savings_leaf,
+                  size: 100,
+                color: Colors.green[700],
+              ),
+          
+              const SizedBox(height: 50),
+          
+              // App Name
+              Text(
+                'FYLLENS',
+                style: TextStyle(
+                  color: Colors.green[700],
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 20,
+                ),
+              ),
+
+              const SizedBox(height: 25),
+          
+              // forgot password
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+                decoration: BoxDecoration(
+                  color: Colors.green.shade700,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    const SizedBox(height: 25),
+
+                    // username textfield
+                    GreenTextfield(
+                      controller: usernameController,
+                      obscureText: false,
+                      hintText: "Username",
+                    ),
+
+                    const SizedBox(height: 15),
+
+                    // password textfield
+                    GreenTextfield(
+                      controller: passwordController,
+                      obscureText: true,
+                      hintText: "Password",
+                    ),
+
+                    const SizedBox(height: 10),
+
+                    // Forgot password or signup link
+                    Row(
+                      children: [
+                        const SizedBox(width: 260),
+
+                        TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Forgot Password?',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 15),
+
+                    // sign in button
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 160),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green.shade700,
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                              side: BorderSide(
+                                color: Colors.white, // 👈 border color
+                                width: 1,            // 👈 border thickness
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 10),
+
+                    // not a member? register now
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Don\'t have an account? Register now!',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+
+                    const SizedBox(height: 70)
+
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
